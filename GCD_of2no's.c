cfea5b4a2c934f039476d6f52228f2314871a828
dharshangnayak-hub <dharshangnayak@gmail.com>
2026-04-28 11:56:19 +0530

    Implement GCD, LCM, and Co-Prime checks

--- GCD_of2no's.c
+++ GCD_of2no's.c
@@ -1,1 +1,82 @@
+//GCD of Two Numbers using Euclidean Method
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("GCD = %d\n", a);
+    return 0;
+}
+
+//GCD of Two Numbers using Recursion
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(a % b, b);
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("GCD = %d\n", gcd(a, b));
+    return 0;
+}
+
+//Check Whether Two Numbers are Co-Prime
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(a % b, b);
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (gcd(a, b) == 1)
+        printf("The numbers are Co-Prime\n");
+    else
+        printf("The numbers are NOT Co-Prime\n");
+
+    return 0;
+}
+
+//LCM of Two Numbers using GCD
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(a % b, b);
+}
+
+int main() {
+    int a, b, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    lcm = (a * b) / gcd(a, b);
+
+    printf("LCM = %d\n", lcm);
+    return 0;
+}
 
